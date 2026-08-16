ec765aacdffdd622333b29ad69d0383c3909427f
UmarSalihuBaba <141918046+UmarSalihuBaba@users.noreply.github.com>
2026-08-16 14:10:23 +0100

    Delete student-portal directory

--- student-portal/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/auth.php';
-$user = current_user();
-header('Location: ' . ($user ? 'dashboard.php' : 'login.php'));
-exit;
--- student-portal/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/auth.php';
-start_secure_session();
-$_SESSION = [];
-session_destroy();
-header('Location: login.php');
-exit;
